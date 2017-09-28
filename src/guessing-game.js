ec49ed24ee5a170778aa58b2dd2f8914d5082c4e
lkush <l.kushnariova@yandex.ru>
2017-09-28 22:57:44 +0300

    Done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max
     }
 
     guess() {
-
+        this.guessed = Math.round((this.max + this.min) / 2);
+        return this.guessed
     }
 
     lower() {
-
+        this.max = this.guessed
     }
 
     greater() {
-
+        this.min = this.guessed
     }
 }
 
